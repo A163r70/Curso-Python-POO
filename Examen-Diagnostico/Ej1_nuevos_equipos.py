1b13a065361476093e6effd38e7ad15453d2333e
AlbertoSC2 <albertodesalinas09@.com>
2025-03-04 16:01:12 -0600

    Examen diagnóstico

--- Examen-Diagnostico/Ej1_nuevos_equipos.py
+++ Examen-Diagnostico/Ej1_nuevos_equipos.py
@@ -1,26 +1,20 @@
 """
 Alumno: Jesús Alberto Ramírez Salinas
 Fecha: 04 de marzo de 2025
-Descripción: Ejercici del examen diagnóstico.
+Descripción: Ejercicio del examen diagnóstico.
 """
-from random import choice
 
-equipos = {'Los_Algoritmos_Anarquistas':{"Hector", "Alberto", "Addi",},
-           'Los_Hackers_de_Cafe':{"Tania", "Patricia", "Rebeca"},
-           'Los_Codificadores_nocturnos':{"Jamileth", "Bryan", "Rosalinda"},
-           'Los_Ctrl+Z':{"Galilea", "Jennifer", "Juan"}}
 
-alumnos = ["Hector", "Alberto", "Addi", "Tania", "Patricia", "Rebeca", "Jamileth", "Bryan", "Rosalinda",
-           "Galilea", "Jennifer", "Juan"]
-equipo1 = []
-equipo2 = []
-equipo3 = []
-equipo4 = []
-equipo5 = []
-equipo6 = []
+alumnos = {"Hector", "Alberto", "Addi", "Tania", "Patricia", "Rebeca", "Jamileth", "Bryan", "Rosalinda",
+           "Galilea", "Jennifer", "Juan"}
 
-for i in range(6):
-    for j in range(2):
-        alumno1 = choice(alumnos)
+cantidad = len(alumnos)
+
+equipos_formados = [ ]
+
+for i in range(len(alumnos):
+    for j in range(i + 1, len(alumnos)):
+        equipos_formados.append((alumnos[i], alumnos[j]))
+
 
 
